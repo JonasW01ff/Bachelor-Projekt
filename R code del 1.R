475d0d138cc9bbb2dc4fea6aa59e7ab9848d193f
JonasW01ff <jw@math.ku.dk>
2022-05-17 13:31:25 +0200

    changes

--- R code del 1.R
+++ R code del 1.R
@@ -6,16 +6,16 @@ library("numDeriv")
 library("matlib")
 #library("comprehenr")
 #library("Hmisc")
-
+"HrejadssdasadskaJKSjlklkHk"
 logLikeFunc <- function(X,kappa,theta,sigma2){
   "
-  Tager daglig data og retunere loglikelihood funktionsværdien,
-  dvs. $\Delta$ t er antaget at være 1, og vi måler i dage.
+  Tager daglig data og retunere loglikelihood funktionsv?rdien,
+  dvs. $\Delta$ t er antaget at v?re 1, og vi m?ler i dage.
   
   ----
   param X
-  ::Type list af numerics eller én numeric
-    En liste eller ét tal med observation(erne/en)
+  ::Type list af numerics eller ?n numeric
+    En liste eller ?t tal med observation(erne/en)
   
   param kappa
   ::Type numeric
@@ -30,7 +30,7 @@ logLikeFunc <- function(X,kappa,theta,sigma2){
     sigma^2 parameteren som et tal
   
   return
-  ::Type list af numerics eller én numeric
+  ::Type list af numerics eller ?n numeric
     funktionsvaerdi(erne/en)
   
   "
@@ -48,11 +48,11 @@ logLikeFunc <- function(X,kappa,theta,sigma2){
 KvadratiskTeststoerelse <- function(logLikeFunc,X,kappa,theta,sigma2){
   "
   Givet en loglikelihood funktion og observationerne, findes
-  den kvadratiske score test størelse
+  den kvadratiske score test st?relse
   
   ----
   param X
-  ::Type list af numerics eller én numeric
+  ::Type list af numerics eller ?n numeric
     En liste med observationerne
   
   param kappa
@@ -73,7 +73,7 @@ KvadratiskTeststoerelse <- function(logLikeFunc,X,kappa,theta,sigma2){
   
   return
   ::Type numeric
-    Teststørelsen $Q_n$ j.f. Lauritzen2021 afsnit 5.4.2.
+    Testst?relsen $Q_n$ j.f. Lauritzen2021 afsnit 5.4.2.
   "
   S <- mean(numDeriv::grad(logLikeFunc,X,kappa,theta,sigma2))
   Ii <- matlib::inv(mean(numDeriv::hessian(logLikeFunc,X,kappa,theta,sigma2)))
