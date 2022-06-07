f531bd866f81b01f166d8f6a0ecb2dce84324a94
JonasW01ff <jw@math.ku.dk>
2022-06-07 13:33:15 +0200

    lidt nyt update

--- R code del 1.R
+++ R code del 1.R
@@ -575,8 +575,41 @@ MLE <- function(X){
   hat_sigma2 <- 2*hat_kappa*hat_v/(1-exp(-2*hat_kappa))
   return(c(hat_kappa,hat_theta,hat_sigma2))
 }
+MLE_GBM <- function(X){
+  "
+  Tager data, og giver MLE for OU-process tilbage
+  
+  
+  -----
+  :param X:
+    Type vector
+      Vector med floats der er din data
+  
+  :return:
+    Type vector
+      Returnere vector med dine tre MLE'er i rækkefølgen
+      beta, sigma^2
+  "
+  
+  data <- X
+  X_1 <- head(X,-1)
+  X <- tail(X,-1)
+  x0 <- sum(X)
+  x1 <- sum(X_1)
+  x01 <- sum(X*X_1)
+  x11 <- sum(X_1*X_1)
+  
+  hat_b <- 0#sum(X-X_1*(x01/x11) )/sum(1-X_1*(x1/x11) )
+  hat_a <- (x01-x1*hat_b)/x11
+  hat_v <- mean((X-X_1*hat_a-hat_b)^2)
+  
+  hat_kappa <- -log(hat_a)
+  hat_theta <- hat_b/(1-exp(-hat_kappa))
+  hat_sigma2 <- 2*hat_kappa*hat_v/(1-exp(-2*hat_kappa))
+  return(c(-hat_kappa,hat_sigma2))
+}
 
-MLE95(X_SPX)
+MLE_GBM(X_SPX)
 
 window <- 21
 lambda <- 0.94
@@ -595,32 +628,195 @@ for (i in 1:2){
   MLE_sigma_rolling_month <- runner(
     var,
     k=window,
-    f= function(X){sqrt(MLE(X)[3])}
+    f= function(X){sqrt(MLE_GBM(X)[2])}
   )
 
   returns <-  c(NaN,tail(var,-1)/head(var,-1))
   EWMA <- runner(
     MLE_sigma_rolling_month,
     k=300,
-    lag=-300+window,
+    lag=-window,
     f=function(X){
+      X <- rev(X)
       out <- 0
       for (i in 1:(window-1+1)){
-        out_ <- lambda*out+(1-lambda)*X[i]^2
-        if (!is.na(out_)){out <- out_}
+        out <- lambda*out+(1-lambda)*X[i]^2
+        #if (!is.na(out_)){out <- out_}
       }
       return(sqrt(out))
     }
   )
   
   #plot(MLE_sigma_rolling_month)
-  mean(MLE_sigma_rolling_month,na.rm=TRUE)
+  print(c("rolling: ",mean(MLE_sigma_rolling_month,na.rm=TRUE)))
+  print(c("EMVA",mean(EWMA, na.rm=TRUE)))
   
   #plot(EWMA)
   
   data <- data.frame(cbind(MLE_sigma_rolling_month,EWMA))
-  data["dag"] = index(VIX)
-  data <- melt(data ,  id.vars = 'dag', variable.name = 'variabel', value.name = valname)
-  if(i==1){print(ggplot(data, aes(dag,SP500)) + geom_line(aes(colour = variabel)))}
-  if(i==2){print(ggplot(data, aes(dag,VIX)) + geom_line(aes(colour = variabel)))}  
+  if(i==1){
+    data["dag"] = index(X_SPX)
+    data <- melt(data ,  id.vars = 'dag', variable.name = 'variabel', value.name = "SP500")
+    print(ggplot(data, aes(dag,SP500)) + geom_line(aes(colour = variabel)))
+    }
+  if(i==2){
+    data["dag"] = index(X_VIX)
+    data <- melt(data ,  id.vars = 'dag', variable.name = 'variabel', value.name = "VIX")
+    print(ggplot(data, aes(dag,VIX)) + geom_line(aes(colour = variabel)))
+    }  
 }
+
+# Split data into buisness months i.e. 21 days non overlapping
+
+BlackScholesFormula  <- function (spot,timetomat,strike,r, q=0, sigma, opttype=1, greektype=1)
+{ 
+  "
+  Docstring skrevet af Jonas Wolff
+  
+  -----
+  param spot
+  ::Type float
+    Start kurs
+  
+  param timetomat
+  ::Type float
+    løbetiden
+    
+  param strike
+  ::Type float
+    Strike
+    
+  param r
+  ::Type float
+    risikofri rente
+  
+  param q
+  ::Type float
+    tids diskontering på param. spot
+    
+  param sigma
+  ::Type float
+    sigma i blackschols (i.e. wienerprocessens koeficient)
+    
+  param opttype
+  ::Type float
+    =1 er det call
+  
+  param greektype
+  ::Type float
+    =1 er det prisen
+    =2 er det tid t delta'et
+    
+  return
+  ::Type float
+    Blacksholes formel output
+    
+  
+  "
+  #opttype <- 3
+  d1<-as.numeric((log(spot/strike)+ ((r-q)+0.5*sigma^2)*timetomat)/(sigma*sqrt(timetomat)))
+  d2<-as.numeric(d1-sigma*sqrt(timetomat))
+  # spot = Y_o
+  # timetomat = T-t
+  # q = (beta-alpha)
+  if (opttype==1 && greektype==1) result<-spot*exp(-q*timetomat)*pnorm(d1)-strike*exp(-r*timetomat)*pnorm(d2)
+  
+  if (opttype==2 && greektype==1) result<-spot*exp(-q*timetomat)*pnorm(d1)-strike*exp(-r*timetomat)*pnorm(d2)-spot*exp(-q*timetomat)+strike*exp(-r*timetomat)
+  
+  if (opttype==1 && greektype==2) result<-exp(-q*timetomat)*pnorm(d1)
+  
+  if (opttype==2 && greektype==2) result<-exp(-q*timetomat)*(pnorm(d1)-1)
+  
+  if (greektype==3) result<-exp(-q*timetomat)*dnorm(d1)/(spot*sigma*sqrt(timetomat))
+  
+  if (greektype==4) result<-exp(-q*timetomat)*spot*dnorm(d1)*sqrt(timetomat)
+  
+  
+  if (opttype==3 && greektype==1) result<- 1*exp(-r*(timetomat))*pnorm(log(strike/spot),(r-1/2*sigma^2)*timetomat,sigma*sqrt(timetomat))
+  if (opttype==3 && greektype==2) result<- -1*exp(-r*(timetomat))*dnorm(log(strike/spot),(r-1/2*sigma^2)*timetomat,sigma*sqrt(timetomat))/spot
+  return(result)
+  BlackScholesFormula<-result
+  
+}
+
+BlackScholesImpVol  <- function (obsprice,spot,timetomat,strike,r, q=0, opttype=1)
+{ difference<- function(sigBS, obsprice,spot,timetomat,strike,r,q,opttype)
+{BlackScholesFormula (spot,timetomat,strike,r,q,sigBS, opttype,1)-obsprice
+}
+uniroot(difference, c(10^-6,10),obsprice=obsprice,spot=spot,timetomat=timetomat,strike=strike,r=r,q=q,opttype=opttype)$root
+}
+
+
+par(mfrow=c(1,1))
+hedge_func <- function(data){ #Tyv stjålet fra rolfs kode
+  #S0<-100
+  S0 <- data[,1]-data[,1]+1000#as.numeric(St[1])
+  r <- 0#0.02
+  mu <- 0#MLE_GBM(data)[1]#0.02
+  
+  sigma <- 20.01
+  sigma_hedge <- 20.01/sqrt(21)#MLE_GBM(X)[2]
+  
+  capT<-1
+  strike<-1000
+  
+  Nhedge<-length(data[1,])-1#10#252
+
+  Nrep<-length(data[,1])#1000  
+  
+  St<-data[,1]-data[,1]+1000#rep(S0, length=Nrep)
+  dt<-capT/Nhedge
+  initialoutlay<-BlackScholesFormula(S0,capT,strike, r,0,sigma_hedge,1,1)
+  #initialoutlay<-BlackScholesFormula(S0,capT,strike, r,0,sigma,1,1)
+  
+  Vpf<- initialoutlay #rep(initialoutlay,length=Nrep)
+  print(Vpf)
+  
+  a<-BlackScholesFormula(St,capT,strike, r,0,sigma_hedge,1,2)
+  b<-Vpf-a*St
+  
+  for(i in 2:Nhedge){
+    #St<-St*exp((mu-0.5*sigma^2)*dt +sigma*sqrt(dt)*rnorm(Nrep))	
+    St <- data[,i]-data[,1]+1000
+    Vpf<-a*St+b*exp(dt*r)    
+    a<-BlackScholesFormula(St,(capT-(i-1)*dt),strike, r,0,sigma_hedge,1,2)
+    b<-Vpf-a*St
+  }
+  
+  #St<-St*exp((mu-0.5*sigma^2)*dt +sigma*sqrt(dt)*rnorm(Nrep))
+  St <- data[,Nhedge+1]-data[,1]+1000
+  Vpf<-a*St+b*exp(dt*r)
+  optionpayoff<-pmax(St-strike,0)
+  hedgeerror<-Vpf-optionpayoff
+  #print(hedgeerror)
+  
+  
+  # SUMMARY STATS & GRAPHS
+  # ======================
+  
+  #print(paste("Initial investment =",round(initialoutlay,4)))
+  print(paste("Average discounted option payoff =",round(exp(-r*capT)*mean(optionpayoff),4)))
+  print(paste("Average discounted portfolio value =",round(exp(-r*capT)*mean(Vpf),4)))
+  print(paste("Terminal Hedge error mean =",mean(abs(hedgeerror))))
+  print(paste("Terminal Hedge error SD =",sd(hedgeerror)))
+  
+  ToFile<-FALSE
+  
+  if (ToFile) png(file="Scatter.png",res=300,width = 3*480, height = 3*480)
+  plot(St,Vpf,col="blue",xlab="S(T)",ylab="Value of hedge portfolio",main="Discrete hedging of a call-option",xlim=c(50,1200),ylim=c(-5,105))
+  text(50,100,paste("# hegde points =",Nhedge),adj=0)
+  #text(50,95,paste("r-mu =",r-mu),adj=0)
+  #text(50,90,paste("sigma-sigma_hedge =",sigma-sigma_hedge),adj=0)
+  points(50:1200,pmax(50:1200 - strike,0),type='l',lwd=3)
+  
+  if (ToFile) dev.off()
+  
+}
+
+bmonth <- data.frame(split(tail(X_SPX,length(X_SPX)%%21*(-1)),1:21)) #bmonth[1,] would give you the first buisness month
+
+hedge_func(bmonth)
+
+
+
+
